58d07f47143cc06c33fe51d022100828cb64e0c5
Pawel Wujczyk <pwujczyk@gmail.com>
2025-08-20 18:44:39 +0200

    install module

--- UpdatePhotos.ps1
+++ UpdatePhotos.ps1
@@ -2,6 +2,7 @@ $SourceDirectory="e:\Prism\PrismPhoto\"
 $DestinationDirectory="\\192.168.0.41\Prism\PrismPhoto\"
 Robocopy.exe $SourceDirectory $DestinationDirectory /MIR /DCOPY:T /e /copy:DAT /mt 
 
+Install-Module ProductivityTools.DirectoryReverseOrder  -Scope CurrentUser -Force -AcceptLicense
 cd $SourceDirectory
 Remove-PrefixFromDirectoryName
 Set-DirectoryInReverseOrder
