1d9e0c96b3899e1e7b3b20b69701d326f36253e9
Pawel Wujczyk <pwujczyk@gmail.com>
2026-08-30 22:23:41 +0200

    udpate scripts

--- UpdateDB.sh
+++ UpdateDB.sh
@@ -26,24 +26,27 @@ fi
 
 echo "Connecting to database at $DB_HOST using $DB_CLIENT..." | tee -a "$LOG_FILE"
 
-$DB_CLIENT -h "$DB_HOST" --user="root" --password="$ENV_PHOTOPRISM_DATABASE_PASSWORD" --database="photoprism" --execute="update albums a
+$DB_CLIENT -h "$DB_HOST" --user="root" --password="$ENV_PHOTOPRISM_DATABASE_PASSWORD" --database="photoprism" --execute="
+update albums a
 inner join photos p on a.album_path=p.photo_path
 inner join files f on p.id=f.photo_id
 set thumb=convert(f.file_hash, char(200)),thumb_src='manual', album_order='name'
 where convert(album_type, char(100))='folder'
 and convert(p.photo_name, char(200)) like '%Cover%';"
 
-$DB_CLIENT -h "$DB_HOST" --user="root" --password="$ENV_PHOTOPRISM_DATABASE_PASSWORD" --database="photoprism" --execute="update albums
-set album_category='';
+$DB_CLIENT -h "$DB_HOST" --user="root" --password="$ENV_PHOTOPRISM_DATABASE_PASSWORD" --database="photoprism" --execute="
+update albums
+set album_category='';"
 
-
+$DB_CLIENT -h "$DB_HOST" --user="root" --password="$ENV_PHOTOPRISM_DATABASE_PASSWORD" --database="photoprism" --execute="
 DELETE FROM albums 
 WHERE album_type = 'folder' 
-  AND album_path NOT IN (SELECT DISTINCT photo_path FROM photos WHERE photo_path IS NOT NULL AND photo_path <> '');
+  AND album_path NOT IN (SELECT DISTINCT photo_path FROM photos WHERE photo_path IS NOT NULL AND photo_path <> '');"
 
+$DB_CLIENT -h "$DB_HOST" --user="root" --password="$ENV_PHOTOPRISM_DATABASE_PASSWORD" --database="photoprism" --execute="
+UPDATE albums set album_category='';"
 
-UPDATE albums set album_category='';
-
+$DB_CLIENT -h "$DB_HOST" --user="root" --password="$ENV_PHOTOPRISM_DATABASE_PASSWORD" --database="photoprism" --execute="
 UPDATE albums 
 SET album_category = SUBSTRING_INDEX(album_path, '#', -1) 
 WHERE album_type = 'folder' 
@@ -54,7 +57,7 @@ WHERE album_type = 'folder'
       FROM photos 
       WHERE photo_path IS NOT NULL 
         AND photo_path <> ''
-  );
+  );"
 
 echo "$(date): Script finished" | tee -a "$LOG_FILE"
 
